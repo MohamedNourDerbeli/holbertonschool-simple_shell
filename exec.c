f4beb8ed6501421d19417ef4032259dd295b5137
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2023-12-15 21:15:00 +0100

    exec command

---
+++ exec.c
@@ -0,0 +1,29 @@
+#include "shell.h"
+/**
+ * exec - exec
+ * @command: char
+ * @av: char
+ * Return: Always 0.
+ */
+int exec(char **command, char **av)
+{
+	pid_t child;
+	int status;
+
+	child = fork();
+	if (child == 0)
+	{
+		if (execve(command[0], command, environ) == -1)
+		{
+			perror(av[0]);
+			free_array(command);
+			exit(0);
+		}
+	}
+	else
+	{
+		waitpid(child, &status, 0);
+		free_array(command);
+	}
+	return (WEXITSTATUS(status));
+}
