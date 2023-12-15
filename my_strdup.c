906cf9d3d898bd83979b89ca6b24984539eeca8d
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2023-12-15 21:23:59 +0100

    string dup

--- my_strdup.c
+++ my_strdup.c
@@ -4,21 +4,16 @@
  * @s: const char
  * Return: Always 0.
  */
+
 char *my_strdup(const char *s)
 {
-	if (s == NULL)
-	{
-		return (NULL);
-	}
-
 	size_t len = strlen(s) + 1;
-	char *dup_str = (char *)malloc(len);
+	char *new_str = (char *)malloc(len);
 
-	if (dup_str == NULL)
-	{
+	if (new_str == NULL)
 		return (NULL);
-	}
 
-	strcpy(dup_str, s);
-	return (dup_str);
+	strcpy(new_str, s);
+
+	return (new_str);
 }
