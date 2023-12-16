a2f93675f1a65f2bc5019d6cfa4aa4631222aa2a
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2023-12-16 22:52:37 +0100

    handle path

---
+++ handle_path.c
@@ -0,0 +1,44 @@
+#include "shell.h"
+
+char *handle_path(char *command)
+{
+	char *env, *path, *cmd;
+	struct stat buf;
+	int i;
+
+	for (i = 0; command[i]; i++)
+	{
+		if (command[i] == '/')
+		{
+			if (stat(command, &buf) == 0)
+				return (my_strdup(command));
+			return (NULL);
+		}
+	}
+
+	env = _get_env("PATH");
+	if (!env)
+		return (NULL);
+
+	path = strtok(env, ":");
+	while (path)
+	{
+		cmd = malloc(strlen(path) + strlen(command) + 2);
+		if (cmd)
+		{
+			strcpy(cmd, path);
+			strcat(cmd, "/");
+			strcat(cmd, command);
+			if (stat(cmd, &buf) == 0)
+			{
+				free(env);
+				return (cmd);
+			}
+			free(cmd), cmd = NULL;
+
+			path = strtok(NULL, ":");
+		}
+	}
+	free(path);
+	return (NULL);
+}
