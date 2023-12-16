9827d5dce7d4dfb8e145dc36d3923ea005fb7fc3
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2023-12-16 23:53:35 +0100

    exec command

--- exec.c
+++ exec.c
@@ -5,26 +5,33 @@
  * @av: char
  * Return: Always 0.
  */
-int exec(char **command, char **av)
+int exec(char **command, char **av, int idx)
 {
 	pid_t child;
 	int status;
-
+	char *path;
+	path = handle_path(command[0]);
+	if (!path)
+	{
+		fprintf(stderr, "%s: %d: %s: not found", av[0], idx, command[0]);
+		free_array(command);
+		return (127);
+	}
 
 	child = fork();
 	if (child == 0)
 	{
-		if (execve(handle_path(command[0]), command, environ) == -1)
+		if (execve(path, command, environ) == -1)
 		{
-			perror(av[0]);
+			free(path), path = NULL;
 			free_array(command);
-			exit(EXIT_FAILURE);
 		}
 	}
 	else
 	{
 		waitpid(child, &status, 0);
 		free_array(command);
+		free(path), path = NULL;
 	}
 
 	return (WEXITSTATUS(status));
