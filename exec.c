524f75c6461f6af7bef5d20db3574aaadda0890e
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2023-12-16 19:54:43 +0100

    exec command

--- exec.c
+++ exec.c
@@ -14,17 +14,17 @@ int exec(char **command, char **av)
 	child = fork();
 	if (child == 0)
 	{
-
-		if (execve(command[0], command, environ) == -1 && strcmp(command[0], "exit") == -1)
-		{
-			perror(av[0]);
-			free_array(command);
-			exit(0);
-		}
 		if (strcmp(command[0], "exit") == 0)
 		{
 			free_array(command);
 			exit(0);
+		}
+
+		if (execve(command[0], command, environ) == -1)
+		{
+			perror(av[0]);
+			free_array(command);
+			exit(2);
 		}
 	}
 	else
