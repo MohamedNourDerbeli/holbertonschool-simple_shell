f44b561789fc2cad871d7617240184bbdbaf0211
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2023-12-17 20:22:20 +0100

    handle built in

--- built-in.c
+++ built-in.c
@@ -7,20 +7,30 @@ int is_built_in(char *command)
 	int i;
 	for (i = 0; built[i]; i++)
 	{
-		if (strcmp(command,built[i])==0)
+		if (strcmp(command, built[i]) == 0)
 		{
 			return (1);
 		}
 	}
 	return (0);
 }
-void handle_built_in(char **command, int status,int idx)
+void handle_built_in(char **command, int status, int idx)
 {
+	int i;
 
 	(void)idx;
 	if (strcmp(command[0], "exit") == 0)
 	{
 		free_array(command);
 		exit(status);
+	}
+	else if (strcmp(command[0], "env") == 0)
+	{
+		for (i = 0; environ[i]; i++)
+		{
+			printf("%s\n", environ[i]);
+		}
+		free_array(command);
+		(status) = 0;
 	}
 }
