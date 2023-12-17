fd0dc3180b12b4e43cc3d359777cc98392ec43f2
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2023-12-17 02:37:34 +0100

    get env

--- getenv.c
+++ getenv.c
@@ -3,14 +3,15 @@ char *_get_env(char *var)
 {
 	char *tmp, *value, *key, *env;
 	int i;
+
 	for (i = 0; environ[i]; i++)
 	{
 		tmp = my_strdup(environ[i]);
 		key = strtok(tmp, "=");
-		if (strcmp(key, var) == 0)
+		if (key != NULL && strcmp(key, var) == 0)
 		{
 			value = strtok(NULL, "\n");
-			env = my_strdup(value);
+			env = (value != NULL) ? my_strdup(value) : NULL;
 			free(tmp);
 			return (env);
 		}
