a606a2195379615315e4274316fef3774b4fc127
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2023-12-19 18:36:58 +0100

    built in file

--- built-in.c
+++ built-in.c
@@ -3,7 +3,7 @@
 int is_built_in(char **command)
 
 {
-	char *built[] = {"exit","env", NULL};
+	char *built[] = {"exit", "env", "cd", "help", NULL};
 	int i;
 	for (i = 0; built[i]; i++)
 	{
@@ -14,23 +14,57 @@ int is_built_in(char **command)
 	}
 	return (0);
 }
-void handle_built_in(char **command, int *status, int idx)
+void handle_built_in(char **command, int *status, int idx, char **av)
 {
-	int i;
-
-	(void)idx;
 	if (strcmp(command[0], "exit") == 0)
 	{
-		free_array(command);
-		exit(*status);
+		hsh_exit(command, status);
 	}
 	else if (strcmp(command[0], "env") == 0)
 	{
-		for (i = 0; environ[i]; i++)
-		{
-			printf("%s\n", environ[i]);
-		}
-		free_array(command);
-		(*status) = 0;
+		hsh_env(command, status);
 	}
+	else if (strcmp(command[0], "cd") == 0)
+	{
+		hsh_cd(command, status, idx, av);
+	}
+}
+
+void hsh_exit(char **cmd, int *status)
+{
+	free_array(cmd);
+	exit(*status);
+}
+void hsh_env(char **cmd, int *status)
+{
+	int i;
+
+	for (i = 0; environ[i]; i++)
+	{
+		printf("%s\n", environ[i]);
+	}
+	free_array(cmd);
+	(*status) = 0;
+}
+void hsh_cd(char **cmd, int *status, int idx, char **av)
+{
+	(*status) = 0;
+	if (strcmp(cmd[1], "") == 0)
+	{
+		if (chdir("/") == -1)
+		{
+			fprintf(stderr, "%s: %d: %s: not found\n", av[0], idx, cmd[0]);
+			(*status) = 0;
+		}
+	}
+	else
+	{
+		if (chdir(cmd[1]) == -1)
+		{
+			fprintf(stderr, "%s: %d: %s: can't cd to %s\n", av[0], idx, cmd[0], cmd[1]);
+			(*status) = 0;
+		}
+	}
+
+	free_array(cmd);
 }
