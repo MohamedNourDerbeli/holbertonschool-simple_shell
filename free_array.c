74dff8ef58f912e59d5a6ce5a03a9f6762d6c2fd
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2023-12-30 18:04:37 +0100

    add comment for the code

--- free_array.c
+++ free_array.c
@@ -1,17 +1,20 @@
 #include "shell.h"
 /**
- * free_array - free 2d array
- * @command: char
- * Return: Always 0.
+ * free_array - Frees a dynamically allocated 2D array of strings.
+ *
+ * @command: The 2D array of strings to be freed.
  */
 void free_array(char **command)
 {
 	int i;
 
+	/*Check if the input array is NULL*/
 	if (!command)
 		return;
-
+	/*Iterate through the rows of the array*/
 	for (i = 0; command[i]; i++)
+		/*Free the memory allocated for each string in the row*/
 		free(command[i]), command[i] = NULL;
+	/*Free the memory allocated for the array of pointers*/
 	free(command), command = NULL;
 }
