d6784e93cbc39f42b247611cb6f7b1e5cd748253
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2023-12-30 17:49:14 +0100

    add comment for the code

--- read_line.c
+++ read_line.c
@@ -1,7 +1,9 @@
 #include "shell.h"
 /**
- * read_line - read line
- * Return: Always 0.
+ * read_line - Reads a line of input from the user.
+ *
+ * Return: A pointer to the buffer containing the input line.
+ *         Returns NULL on failure or when encountering EOF.
  */
 
 char *read_line(void)
@@ -9,15 +11,18 @@ char *read_line(void)
 	ssize_t n;
 	size_t len = 0;
 	char *buf = NULL;
-
+	/*Print a shell prompt if the input is from a terminal*/
 	if (isatty(STDIN_FILENO))
 		write(STDOUT_FILENO, "$ ", 2);
-
+	/* Use getline to read a line of input*/
 	n = getline(&buf, &len, stdin);
+	/*Check for errors or encountering EOF*/
 	if (n == -1)
 	{
+		/*Free allocated memory and set buffer to NULL*/
 		free(buf), buf = NULL;
 		return (NULL);
 	}
+	/*Return the buffer containing the input line*/
 	return (buf);
 }
