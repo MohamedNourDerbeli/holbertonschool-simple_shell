0f4bdd57c227d183b439c9b884f759bd3f0e0ab4
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2023-12-30 17:52:49 +0100

    add comment for the code

--- split_str.c
+++ split_str.c
@@ -1,22 +1,26 @@
 #include "shell.h"
 /**
- * split_str - shell
+ * split_str - Tokenizes a string into an array of strings.
  *
- * @line: char
- * Return: Always 0.
+ * @line: The string to be tokenized.
+ * Return: An array of strings (tokens).
+ *         Returns NULL on failure or when @line is NULL.
  */
 
 char **split_str(char *line)
 {
 	char *token, *delim = " \t\n", *tmp = NULL, **command = NULL;
 	int count = 0, i = 0;
-
+	/* Check if the input string is NULL*/
 	if (!line)
 		return (NULL);
+	/*Duplicate the input string to avoid modifying the original*/
 	tmp = my_strdup(line);
+	/*Tokenize the duplicated string to count the number of tokens*/
 	token = strtok(tmp, delim);
 	if (token == NULL)
 	{
+		/*Free allocated memory and set pointers to NULL*/
 		free(line), line = NULL;
 		free(tmp), tmp = NULL;
 		return (NULL);
@@ -27,14 +31,17 @@ char **split_str(char *line)
 		count++;
 		token = strtok(NULL, delim);
 	}
+	/*Free the duplicated string*/
 	free(tmp), tmp = NULL;
+	/*Allocate memory for the array of strings (tokens)*/
 	command = malloc(sizeof(char *) * (count + 1));
 	if (!command)
 	{
+		/*Free original line and return NULL in case of memory allocation failure*/
 		free(line), line = NULL;
 		return (NULL);
 	}
-
+	/*Tokenize the original string and copy tokens to the array*/
 	token = strtok(line, delim);
 	while (token)
 	{
@@ -42,7 +49,10 @@ char **split_str(char *line)
 		token = strtok(NULL, delim);
 		i++;
 	}
+	/*Free the original line and set the last element of the array to NULL*/
 	free(line), line = NULL;
 	command[i] = NULL;
+	/* Return the array of strings (tokens)
+*/
 	return (command);
 }
